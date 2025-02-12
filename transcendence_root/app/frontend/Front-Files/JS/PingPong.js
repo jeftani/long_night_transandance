cb54ae59056a1364657cc64516269311a016b2c0
ajeftani <abdallahjeff92@gmail.com>
2025-02-12 03:25:20 +0100

    Automated commit 2025-02-12 03:25:20

--- transcendence_root/app/frontend/Front-Files/JS/PingPong.js
+++ transcendence_root/app/frontend/Front-Files/JS/PingPong.js
@@ -19,8 +19,8 @@ function startLocalGame() {
     localCanvas.style.display = 'block';
 
     const ctx = localCanvas.getContext('2d');
-    let player1 = { x: 10, y: 150, width: 10, height: 100, score: 0 };
-    let player2 = { x: 780, y: 150, width: 10, height: 100, score: 0 };
+    let player1 = { x: 10, y: 250, width: 10, height: 100, score: 0 };
+    let player2 = { x: 780, y: 250, width: 10, height: 100, score: 0 };
     let ball = { x: 300, y: 200, radius: 10, dx: 3, dy: 3 };
     let keysPressed = {};
 
